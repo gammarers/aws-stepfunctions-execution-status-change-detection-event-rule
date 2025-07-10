af6ca732324b581df9cd5ec520dbdc9cdc97331c
yicr <yicr@users.noreply.github.com>
2025-07-10 15:32:02 +0900

    fix(test): status filtering

---
+++ test/rule.specific.test.ts
@@ -0,0 +1,43 @@
+import { App, Stack } from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import * as events from 'aws-cdk-lib/aws-events';
+import { StepFunctionsExecutionStatusChangeDetectionEventRule, TargetStatus } from '../src';
+
+describe('Default Rule Check', () => {
+
+  const app = new App();
+  const stack = new Stack(app, 'TestingStack');
+
+  const rule = new StepFunctionsExecutionStatusChangeDetectionEventRule(stack, 'StepFunctionsExecutionStatusChangeDetectionEventRule', {
+    // ruleName: 'step-function-execution-status-change-detection-event-rule',
+    targetStatuses: [TargetStatus.ABORTED, TargetStatus.FAILED, TargetStatus.TIMED_OUT],
+  });
+
+  it('Is Rule', async () => {
+    expect(rule).toBeInstanceOf(events.Rule);
+  });
+
+  const template = Template.fromStack(stack);
+
+  it('Should match event rule.', async () => {
+    template.hasResourceProperties('AWS::Events::Rule', Match.objectEquals({
+      State: 'ENABLED',
+      EventPattern: Match.objectEquals({
+        'source': [
+          'aws.states',
+        ],
+        'detail-type': [
+          'Step Functions Execution Status Change',
+        ],
+        'detail': {
+          status: ['ABORTED', 'FAILED', 'TIMED_OUT'],
+        },
+      }),
+    }));
+  });
+
+  it('Should match snapshot.', async () => {
+    expect(template.toJSON()).toMatchSnapshot();
+  });
+
+});
